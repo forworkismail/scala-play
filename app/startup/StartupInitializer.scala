ae7a89dacc4b2eccfa7fe07c2ee9815eb3771a8c
Mohammad Ismail <forwork.ismail@gmail.com>
2023-07-08 00:36:39 +0430

    feat: inject application lifecycle to StartupInitializer
    Changes to be committed:
    	modified:   app/startup/StartupInitializer.scala

--- app/startup/StartupInitializer.scala
+++ app/startup/StartupInitializer.scala
@@ -1,21 +1,24 @@
 package startup
 
 import play.api.Logging
+import play.api.inject.ApplicationLifecycle
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 @Singleton
-class StartupInitializer @Inject()(databaseInitializer: DatabaseInitializer) extends Logging {
+class StartupInitializer @Inject()(databaseInitializer: DatabaseInitializer, lifecycle: ApplicationLifecycle)
+  extends Logging {
 
   databaseInitializer.createTables().onComplete {
     case Success(_) => logger.info("Database initialization successful.")
     case Failure(exception) => logger.error("Database initialization failed.", exception)
   }
 
-  //  lifecycle.addStopHook { () =>
-  //    logger.info("Application shutdown...")
-  //    Future.successful(())
-  //  }
+  lifecycle.addStopHook { () =>
+    logger.info("Application shutdown...")
+    Future.successful(())
+  }
 }
